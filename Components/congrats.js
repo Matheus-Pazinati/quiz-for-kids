46ce59292a496964d14b3d9b5e34ff9c19d7233b
Matheus Pazinati <matheuspazinati@gmail.com>
2021-10-27 10:07:13 -0300

    Aplica responsividade ao projeto

--- Components/congrats.js
+++ Components/congrats.js
@@ -1,6 +1,6 @@
 const temaParabens = localStorage.getItem("temaMensagem")
 let mensagemParabens = document.querySelector('.mensagem')
-mensagemParabens.textContent = `Você completou o desafio de responder todas as perguntas sobre ${temaParabens}. Agora você pode escolher outro tema, e se desafiar novamente, clicando no botão a baixo.`
+mensagemParabens.textContent = `Você completou o desafio de responder corretamente todas as perguntas sobre ${temaParabens}. Agora você pode escolher outro tema, e se desafiar novamente, clicando no botão a baixo.`
 const botaoMenu = document.querySelector('.botao-menu')
 botaoMenu.addEventListener('click', (event) => {
   event.preventDefault()
