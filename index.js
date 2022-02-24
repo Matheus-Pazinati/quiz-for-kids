e15229ad6e8d863c22dfc46f9b79618a51af72dc
Matheus Pazinati <matheuspazinati@gmail.com>
2022-02-24 13:08:31 -0300

    Refatora codigo que define o tema escolhido

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 import { habilitaJogar } from "./Components/enablePlay.js";
 
 const listaTemas = ["[data-botao-peter]", "[data-botao-alice]", "[data-botao-aladin]", "[data-botao-pinoquio]"]
-for (var i=0; i < listaTemas.length; i++){
-  const botaoTema = document.querySelector(listaTemas[i]);
-  botaoTema.addEventListener("click", habilitaJogar) //Quanto algum dos botoes forem clicados, habilite jogar
-  }
-// -- Inicio da definição do tema para a página de perguntas --
   let botaoEscolhido;
   let temaOpcao;
+
   function defineTema(evento){ //Define a imagem de fundo e as cores dependendo do botão clicado
     evento.preventDefault()
     botaoEscolhido = evento.target.id
     temaOpcao = evento.target.name
   }
-  for (var i=0; i < listaTemas.length; i++){
-    const escolheTema = document.querySelector(listaTemas[i])
-    escolheTema.addEventListener('click', defineTema)
-  }
+
+  listaTemas.forEach((tema) => {
+    const botaoTema = document.querySelector(tema)
+    botaoTema.addEventListener('click', (event) => {
+      habilitaJogar();
+      defineTema(event);
+    })
+  })
+
   const botaoJogar = document.querySelector("[data-botao-jogar]")
   botaoJogar.addEventListener('click', (evento) => { //Transfere para a outra página as informações de fundo e cores
     evento.preventDefault()
